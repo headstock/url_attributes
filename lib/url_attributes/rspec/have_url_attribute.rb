70cfc59e448197e3bf9b4cba6aa84abfff9bc251
George Millo <georgejulianmillo@gmail.com>
2014-11-24 18:12:19 +0700

    bug fix; don't try and load the RSpec matcher unless we're in test mode and RSpec is present

--- lib/url_attributes/rspec/have_url_attribute.rb
+++ lib/url_attributes/rspec/have_url_attribute.rb
@@ -1,19 +1,21 @@
-RSpec::Matchers.define :have_url_attribute do |attribute_name|
-  # This matcher is an ugly piece of shit
-  match do |model|
-    model.send :"#{attribute_name}=", "notavalidurl"
-    model.valid?
-    test_0 = model.errors[attribute_name].include?("is not a valid URL")
-    model.send :"#{attribute_name}=", "http://validurl.com"
-    model.valid?
-    test_1 = !model.errors[attribute_name].include?("is not a valid URL")
-    model.send :"#{attribute_name}=", "nohttp.com"
-    model.run_callbacks :save
-    test_2 = model.send(attribute_name) == "http://nohttp.com"
-    test_0 && test_1 && test_2
-  end
+if Rails.env.test? && defined?(RSpec)
+  RSpec::Matchers.define :have_url_attribute do |attribute_name|
+    # This matcher is an ugly piece of shit
+    match do |model|
+      model.send :"#{attribute_name}=", "notavalidurl"
+      model.valid?
+      test_0 = model.errors[attribute_name].include?("is not a valid URL")
+      model.send :"#{attribute_name}=", "http://validurl.com"
+      model.valid?
+      test_1 = !model.errors[attribute_name].include?("is not a valid URL")
+      model.send :"#{attribute_name}=", "nohttp.com"
+      model.run_callbacks :save
+      test_2 = model.send(attribute_name) == "http://nohttp.com"
+      test_0 && test_1 && test_2
+    end
 
-  failure_message do |model|
-    "expected that #{model} would have a URL attribute called #{attribute_name}, but it doesn't look like a URL"
+    failure_message do |model|
+      "expected that #{model} would have a URL attribute called #{attribute_name}, but it doesn't look like a URL"
+    end
   end
 end
